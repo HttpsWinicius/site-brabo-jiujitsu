9917aec75a1ea62579f47b8365a0a47f43e0b980
elo9000989 <winiciussouzadev@gmail.com>
2023-12-20 21:53:22 -0300

    att pagina horarios - pronta

--- js/scripts-exerteno.js
+++ js/scripts-exerteno.js
@@ -1,38 +1,59 @@
 function mostrarHorarios(filial) {
     if (filial.value == "BM_CASCATINHA") {
+        document.getElementById("texto_cascatinha").style.display = "block"; 
         document.getElementById("horario_bm_cascatinha").style.display = "block";
         document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
         document.getElementById("horario_san_house").style.display = "none";
+        document.getElementById("horario_igreja").style.display = "none";
+        document.getElementById("horario_sistema_degraus").style.display = "none";
     } else if (filial.value == "BM_ENCOSTA_DO_SOL") {
         document.getElementById("horario_bm_encosta_sol").style.display = "block";
+        document.getElementById("texto_cascatinha").style.display = "block"; 
         document.getElementById("horario_bm_cascatinha").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
         document.getElementById("horario_san_house").style.display = "none";
+        document.getElementById("horario_igreja").style.display = "none";
+        document.getElementById("horario_sistema_degraus").style.display = "none";
     } else if (filial.value == "SIMAO_PEREIRA") {
         document.getElementById("horario_simao_pereira").style.display = "block";
-        document.getElementById("horario_bm_encosta_sol").style.display = "none";
+        document.getElementById("texto_cascatinha").style.display = "block"; 
         document.getElementById("horario_bm_cascatinha").style.display = "none";
+        document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_san_house").style.display = "none";
+        document.getElementById("horario_igreja").style.display = "none";
+        document.getElementById("horario_sistema_degraus").style.display = "none";
     } else if (filial.value == "SDE") {
+        document.getElementById("horario_sistema_degraus").style.display = "block";
+        document.getElementById("texto_cascatinha").style.display = "block"; 
         document.getElementById("horario_bm_cascatinha").style.display = "none";
         document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
         document.getElementById("horario_san_house").style.display = "none";
+        document.getElementById("horario_igreja").style.display = "none";
     } else if (filial.value == "IEQ") {
+        document.getElementById("horario_igreja").style.display = "block";
+        document.getElementById("texto_cascatinha").style.display = "block"; 
         document.getElementById("horario_bm_cascatinha").style.display = "none";
         document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
         document.getElementById("horario_san_house").style.display = "none";
+        document.getElementById("horario_sistema_degraus").style.display = "none";
     } else if (filial.value == "SHJF") {
         document.getElementById("horario_san_house").style.display = "block";
-        document.getElementById("horario_simao_pereira").style.display = "none";
-        document.getElementById("horario_bm_encosta_sol").style.display = "none";
+        document.getElementById("texto_cascatinha").style.display = "block"; 
         document.getElementById("horario_bm_cascatinha").style.display = "none";
+        document.getElementById("horario_bm_encosta_sol").style.display = "none";
+        document.getElementById("horario_simao_pereira").style.display = "none";
+        document.getElementById("horario_igreja").style.display = "none";
+        document.getElementById("horario_sistema_degraus").style.display = "none";
     } else {
         document.getElementById("horario_bm_cascatinha").style.display = "none";
         document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
         document.getElementById("horario_san_house").style.display = "none";
+        document.getElementById("horario_igreja").style.display = "none";
+        document.getElementById("horario_sistema_degraus").style.display = "none";
+        document.getElementById("texto_cascatinha").style.display = "none"; 
     }
 }
