77fca5e751826c242f4a867f5b6e88660d2a6209
Winicius Souza <winiciussouzadev@gmail.com>
2024-01-07 15:22:25 -0300

    atualizando index e pagina metodologia infantil

--- js/scripts-exerteno.js
+++ js/scripts-exerteno.js
@@ -1,6 +1,6 @@
 function mostrarHorarios(filial) {
     if (filial.value == "BM_CASCATINHA") {
-        document.getElementById("texto_cascatinha").style.display = "block"; 
+        document.getElementById("texto_cascatinha").style.display = "block";
         document.getElementById("horario_bm_cascatinha").style.display = "block";
         document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
@@ -9,7 +9,7 @@ function mostrarHorarios(filial) {
         document.getElementById("horario_sistema_degraus").style.display = "none";
     } else if (filial.value == "BM_ENCOSTA_DO_SOL") {
         document.getElementById("horario_bm_encosta_sol").style.display = "block";
-        document.getElementById("texto_cascatinha").style.display = "block"; 
+        document.getElementById("texto_cascatinha").style.display = "block";
         document.getElementById("horario_bm_cascatinha").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
         document.getElementById("horario_san_house").style.display = "none";
@@ -17,7 +17,7 @@ function mostrarHorarios(filial) {
         document.getElementById("horario_sistema_degraus").style.display = "none";
     } else if (filial.value == "SIMAO_PEREIRA") {
         document.getElementById("horario_simao_pereira").style.display = "block";
-        document.getElementById("texto_cascatinha").style.display = "block"; 
+        document.getElementById("texto_cascatinha").style.display = "block";
         document.getElementById("horario_bm_cascatinha").style.display = "none";
         document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_san_house").style.display = "none";
@@ -25,7 +25,7 @@ function mostrarHorarios(filial) {
         document.getElementById("horario_sistema_degraus").style.display = "none";
     } else if (filial.value == "SDE") {
         document.getElementById("horario_sistema_degraus").style.display = "block";
-        document.getElementById("texto_cascatinha").style.display = "block"; 
+        document.getElementById("texto_cascatinha").style.display = "block";
         document.getElementById("horario_bm_cascatinha").style.display = "none";
         document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
@@ -33,7 +33,7 @@ function mostrarHorarios(filial) {
         document.getElementById("horario_igreja").style.display = "none";
     } else if (filial.value == "IEQ") {
         document.getElementById("horario_igreja").style.display = "block";
-        document.getElementById("texto_cascatinha").style.display = "block"; 
+        document.getElementById("texto_cascatinha").style.display = "block";
         document.getElementById("horario_bm_cascatinha").style.display = "none";
         document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
@@ -41,7 +41,7 @@ function mostrarHorarios(filial) {
         document.getElementById("horario_sistema_degraus").style.display = "none";
     } else if (filial.value == "SHJF") {
         document.getElementById("horario_san_house").style.display = "block";
-        document.getElementById("texto_cascatinha").style.display = "block"; 
+        document.getElementById("texto_cascatinha").style.display = "block";
         document.getElementById("horario_bm_cascatinha").style.display = "none";
         document.getElementById("horario_bm_encosta_sol").style.display = "none";
         document.getElementById("horario_simao_pereira").style.display = "none";
@@ -54,7 +54,7 @@ function mostrarHorarios(filial) {
         document.getElementById("horario_san_house").style.display = "none";
         document.getElementById("horario_igreja").style.display = "none";
         document.getElementById("horario_sistema_degraus").style.display = "none";
-        document.getElementById("texto_cascatinha").style.display = "none"; 
+        document.getElementById("texto_cascatinha").style.display = "none";
     }
 }
 
@@ -63,12 +63,20 @@ function exibeMetodologias(metodologia) {
     if (metodologia.value == "jj_adulto") {
         document.querySelector('.metodologia_adulto').style.display = "block";
         document.querySelector('.metodologia_infantil').style.display = "none";
+        document.querySelector('.metodologia_feminino').style.display = "none";
+
+    } else if (metodologia.value == "jj_feminino") {
+        document.querySelector('.metodologia_feminino').style.display = "block";
+        document.querySelector('.metodologia_infantil').style.display = "none";
+        document.querySelector('.metodologia_adulto').style.display = "none";
 
     } else if (metodologia.value == "jj_infantil") {
         document.querySelector('.metodologia_adulto').style.display = "none";
         document.querySelector('.metodologia_infantil').style.display = "block";
+        document.querySelector('.metodologia_feminino').style.display = "none";
     } else {
         document.querySelector('.metodologia_adulto').style.display = "none";
         document.querySelector('.metodologia_infantil').style.display = "none";
+        document.querySelector('.metodologia_feminino').style.display = "none";
     }
 }
