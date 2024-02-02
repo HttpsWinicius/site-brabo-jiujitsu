b655baddd24174860cdc42a182c98d34152cb00a
Winicius Souza <winiciussouzadev@gmail.com>
2024-02-02 17:37:04 -0300

    att

--- js/scripts-exerteno.js
+++ js/scripts-exerteno.js
@@ -63,20 +63,12 @@ function exibeMetodologias(metodologia) {
     if (metodologia.value == "jj_adulto") {
         document.querySelector('.metodologia_adulto').style.display = "block";
         document.querySelector('.metodologia_infantil').style.display = "none";
-        document.querySelector('.metodologia_feminino').style.display = "none";
-
-    } else if (metodologia.value == "jj_feminino") {
-        document.querySelector('.metodologia_feminino').style.display = "block";
-        document.querySelector('.metodologia_infantil').style.display = "none";
-        document.querySelector('.metodologia_adulto').style.display = "none";
 
     } else if (metodologia.value == "jj_infantil") {
         document.querySelector('.metodologia_adulto').style.display = "none";
         document.querySelector('.metodologia_infantil').style.display = "block";
-        document.querySelector('.metodologia_feminino').style.display = "none";
     } else {
         document.querySelector('.metodologia_adulto').style.display = "none";
         document.querySelector('.metodologia_infantil').style.display = "none";
-        document.querySelector('.metodologia_feminino').style.display = "none";
     }
 }
