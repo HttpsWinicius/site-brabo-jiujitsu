44ce17821ac5d53bbd33a33680eb9e76b502e173
Winicius Souza <winiciussouzadev@gmail.com>
2024-03-08 06:31:05 -0300

    otimizando site

--- js/scripts.js
+++ js/scripts.js
@@ -62,4 +62,14 @@ $(document).ready(function () {
     });
 });
 
-
+//NavBar dropdown
+$(document).ready(function () {
+    $(".navbar-light .dmenu").hover(
+        function () {
+            $(this).find(".sm-menu").first().stop(true, true).slideDown(150);
+        },
+        function () {
+            $(this).find(".sm-menu").first().stop(true, true).slideUp(105);
+        }
+    );
+});
